ae26ec2a0f394f4cbf3ab9c12c0431da6b5c3e19
Tim van Dijen <tvdijen@gmail.com>
2025-12-16 00:38:36 +0100

    Fix class registry

--- classes/element.registry.php
+++ classes/element.registry.php
@@ -11,4 +11,13 @@ return [
         'PolicyAttachment' => '\SimpleSAML\WebServices\Policy\XML\wsp_200409\PolicyAttachment',
         'PolicyReference' => '\SimpleSAML\WebServices\Policy\XML\wsp_200409\PolicyReference',
     ],
+    'http://www.w3.org/2006/07/ws-policy' => [
+        'All' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\All',
+        'AppliesTo' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\AppliesTo',
+        'ExactlyOne' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\ExactlyOne',
+        'Policy' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\Policy',
+        'PolicyAttachment' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\PolicyAttachment',
+        'PolicyReference' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\PolicyReference',
+        'URI' => '\SimpleSAML\WebServices\Policy\XML\wsp_200607\URI',
+    ],
 ];
